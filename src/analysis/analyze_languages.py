f4bac9a6f3e44c610b9dd4ab913f2d0ad581f3e4
Sean Trott <seantrott@Seans-MacBook-Pro-5.local>
2018-11-08 10:42:13 -0800

    Added analysis script

---
+++ src/analysis/analyze_languages.py
@@ -0,0 +1,86 @@
+"""Read in vectors for each language.
+
+TO DO:
+- Parallelize
+- Ignore/remove .html items in vocab
+- Should we take negative sign of form or meaning distance (to make them compatible / same direction?)
+
+TO SAVE:
+- vocab size
+- results of regression analysis
+- all the actual comparisons? (To use in a full mixed-model analysis)
+
+"""
+
+import random
+
+import pandas as pd 
+import editdistance as ed
+import scipy.stats as ss
+
+from itertools import combinations
+from tqdm import tqdm
+from gensim.models import KeyedVectors
+
+
+
+def contains_html(word):
+	"""Return whether word contains .html characters."""
+	html_characters = ['<', '[', '/', '(', '>']
+	return any(elem in word for elem in html_characters)
+
+def remove_html_words(words):
+	"""Remove all words in list with .html characters."""
+	filtered = [w for w in words if not contains_html(w)]
+
+def compare_form_and_meaning(model, words):
+	"""Compare form and meaning distances of all word pairs."""
+	combos = list(combinations(words, 2))
+
+	analysis = []
+	for w1, w2 in tqdm(combos):
+		orth_distance = ed.eval(w1, w2)
+		meaning_distance = model.similarity(w1, w2)
+		analysis.append({
+			'orthographic_distance': orth_distance,
+			'meaning_similarity': meaning_distance
+			})
+
+	return pd.DataFrame(analysis)
+
+
+
+df_languages = pd.read_csv("data/raw/all_languages.csv")
+
+for language in df_languages['language']:
+	try:
+		print("Trying to load model for {language}...".format(language=language))
+		model = KeyedVectors.load_word2vec_format('data/vectors/{language}.vec'.format(language=language))
+		print("Loaded model for {language}...".format(language=language))
+	except FileNotFoundError:
+		print("File for '{language}' not found! Moving on to the next language.".format(language=language))
+		continue
+	
+	print("Now conducting analysis for systematicity in {language}...".format(language=language))
+
+	vocab = model.vocab.keys()
+	# Preprocess vocab to remove .html characters, etc.
+
+	# Hack for now
+	# words = random.sample(vocab, 10)
+	words = vocab
+	num_words = len(words)
+	df_comparisons = compare_form_and_meaning(model=model, words=words)
+
+	reg = ss.linregress(df_comparisons['orthographic_distance'],
+						df_comparisons['meaning_similarity'])
+
+	output = {'vocab_size': num_words,
+			  'slope': reg.slope,
+			  'intercept': reg.intercept,
+			  'correlation': reg.rvalue,
+			  'pval': reg.pvalue,
+			  'std': reg.stderr}
+
+	df_output = pd.DataFrame.from_dict(output)
+	df_output.to_csv("data/processed/{language}_results.csv".format(language=language))
--- src/data/download_vectors.py
+++ src/data/download_vectors.py
@@ -4,7 +4,7 @@ import pandas as pd
 import urllib as ur
 
 
-def main(path_to_table):
+def main(path_to_table, start_index):
 	"""Load links from .csv file, and save list of languages
 
 	Parameters
@@ -17,7 +17,7 @@ def main(path_to_table):
 	df_languages = pd.read_csv(path_to_table)
 
 	for index, row in df_languages.iterrows():
-		if index >= 84:
+		if index >= start_index:
 			language = row['language']
 			print("Downloading vector for {lan}...".format(lan=language))
 			text_link = row['link']
@@ -35,6 +35,9 @@ if __name__ == "__main__":
 
 	parser.add_argument("--table-path", type=str, dest="path_to_table",
 						default="data/raw/all_languages.csv")
+	parser.add_argument("--start-index", type=int, dest="start_index",
+						default=0)
+	
 	args = vars(parser.parse_args())
 	print(args)
 	main(**args)
