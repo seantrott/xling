5bc04ad501bf07c46bdce128c36a4ce2134d1158
Sean Trott <seantrott@wireless-169-228-116-63.ucsd.edu>
2018-11-08 13:38:27 -0800

    parser

--- src/analysis/analyze_languages.py
+++ src/analysis/analyze_languages.py
@@ -21,6 +21,8 @@ import scipy.stats as ss
 from itertools import combinations
 from tqdm import tqdm
 
+from gensim.models import KeyedVectors
+
 from vector_utils import read_vec_file, compute_cosine_distance, compute_similarity
 
 
@@ -34,23 +36,24 @@ def remove_html_words(words):
 	filtered = [w for w in words if not contains_html(w)]
 	return filtered
 
-def compare_form_and_meaning(vocab_mappings, words):
+def compare_form_and_meaning(model, words):
 	"""Compare form and meaning distances of all word pairs."""
 	combos = list(combinations(words, 2))
 
 	analysis = []
 	for w1, w2 in tqdm(combos):
-		w1_vec = vocab_mappings[w1]
-		w2_vec = vocab_mappings[w2]
-		if len(w1_vec) != len(w2_vec):
-			continue
+		# w1_vec = vocab_mappings[w1]
+		# w2_vec = vocab_mappings[w2]
+		# if len(w1_vec) != len(w2_vec):
+		# 	continue
 		orth_distance = ed.eval(w1, w2)
-		meaning_distance = compute_cosine_distance(w1_vec, w2_vec)
+		# meaning_distance = compute_cosine_distance(w1_vec, w2_vec)
+		meaning_similarity = model.similarity(w1, w2)
 		analysis.append({
 			'w1': w1,
 			'w2': w2,
 			'orthographic_distance': orth_distance,
-			'cosine_distance': meaning_distance
+			'meaning_similarity': meaning_similarity
 			})
 
 	return pd.DataFrame(analysis)
@@ -60,15 +63,11 @@ def compare_form_and_meaning(vocab_mappings, words):
 df_languages = pd.read_csv("data/raw/all_languages.csv")
 
 for language in df_languages['language']:
-	if language != "english":
-		continue
-	print("Trying to load english model")
-	filepath = 'data/vectors/{language}.vec'.format(language=language)
-	vocab_mappings = read_vec_file(filepath)
 	try:
 		print("Trying to load model for {language}...".format(language=language))
 		filepath = 'data/vectors/{language}.vec'.format(language=language)
-		vocab_mappings = read_vec_file(filepath)
+		model = KeyedVectors.load_word2vec_format(filepath)
+		# vocab_mappings = read_vec_file(filepath)
 		print("Loaded model for {language}...".format(language=language))
 	except Exception as e:
 		print(e)
@@ -78,16 +77,19 @@ for language in df_languages['language']:
 	print("Now conducting analysis for systematicity in {language}...".format(language=language))
 
 	# Preprocess vocab to remove .html characters, etc.
-	words = vocab_mappings.keys()
+	words = model.vocab.keys()
+	# words = vocab_mappings.keys()
 	words = remove_html_words(words)
 
 	num_words = len(words)
 	print("Vocab size: {n}".format(n=num_words))
 
-	df_comparisons = compare_form_and_meaning(vocab_mappings=vocab_mappings, words=words)
+	# df_comparisons = compare_form_and_meaning(vocab_mappings=vocab_mappings, words=words)
+
+	df_comparisons = compare_form_and_meaning(model=model, words=words)
 
 	reg = ss.linregress(df_comparisons['orthographic_distance'],
-						df_comparisons['cosine_distance'])
+						df_comparisons['meaning_similarity'])
 
 	output = [{'vocab_size': num_words,
 			  'slope': reg.slope,
@@ -97,7 +99,6 @@ for language in df_languages['language']:
 			  'std': reg.stderr}]
 
 	df_output = pd.DataFrame(output)
-	print(df_output)
 
 	df_output.to_csv("data/processed/{language}_results.csv".format(language=language))
 	df_comparisons.to_csv("data/processed/{language}_comparisons.csv".format(language=language))
