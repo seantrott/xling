72b29e340a9aa06fec62b26d18f596c9077b34c9
Sean Trott <seantrott@Seans-MacBook-Pro-5.local>
2018-11-08 10:31:07 -0800

    download

--- src/data/download_vectors.py
+++ src/data/download_vectors.py
@@ -17,7 +17,7 @@ def main(path_to_table):
 	df_languages = pd.read_csv(path_to_table)
 
 	for index, row in df_languages.iterrows():
-		if index >= 66:
+		if index >= 84:
 			language = row['language']
 			print("Downloading vector for {lan}...".format(lan=language))
 			text_link = row['link']
