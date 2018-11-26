3ece4baf5268034ee20b214447d189f2688c40ee
Sean Trott <seantrott@Seans-MacBook-Pro-6.local>
2018-11-26 10:22:00 -0800

    update

--- src/analysis/analyze_languages.py
+++ src/analysis/analyze_languages.py
@@ -56,12 +56,9 @@ SAMPLING = 'frequency'
 df_languages = pd.read_csv("data/raw/all_languages.csv")
 
 for language in df_languages['language']:
-	outfile_path = "data/processed/analysis_from_random_sample/{language}_results.csv".format(language=language)
-	outfile_path2 = "data/processed/test_analysis/{language}_results.csv".format(language=language)
+	# outfile_path = "data/processed/analysis_from_random_sample/{language}_results.csv".format(language=language)
+	outfile_path = "data/processed/test_analysis/{language}_results.csv".format(language=language)
 	if os.path.exists(outfile_path):
-		print("Already analyzed for {language}".format(language=language))
-		continue
-	if os.path.exists(outfile_path2):
 		print("Already analyzed for {language}".format(language=language))
 		continue
 	try:
@@ -111,5 +108,5 @@ for language in df_languages['language']:
 
 	df_output = pd.DataFrame(output)
 
-	df_output.to_csv("data/processed/test_analysis/{language}_results.csv".format(language=language))
+	df_output.to_csv("data/processed/analysis_from_random_sample/{language}_results.csv".format(language=language))
 	# df_comparisons.to_csv("data/processed/{language}_comparisons.csv".format(language=language))
